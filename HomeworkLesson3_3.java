532bf0b8d19de92a1885072b496a81148b0d2d68
cherep4nov <vadim_1793@mail.ru>
2022-06-09 22:42:10 +0300

    Add homework for lesson3

---
+++ HomeworkLesson3_3.java
@@ -0,0 +1,8 @@
+public class HomeworkLesson3_3 {
+    public static void main (String[] arg) {
+        String str = "23114";
+        String reverse = new StringBuffer(str).reverse().toString();
+            System.out.println(reverse);
+    }
+
+}
